210934f98c83220a86e5f044f3e49ea8fd65636d
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-31 16:15:23 +0400

    xpo

---
+++ CS/XPO/FilterLookup/FilterLookup.Module/BusinessObjects/Accessory.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace FilterLookupListView.Module.BusinessObjects {
+    [DefaultClassOptions]
+    public class Accessory : BaseObject {
+        public Accessory(Session session) : base(session) { }
+        private string accessoryName;
+        public string AccessoryName {
+            get { return accessoryName; }
+            set { SetPropertyValue(nameof(AccessoryName), ref accessoryName, value); }
+        }
+        private bool isGlobal;
+        public bool IsGlobal {
+            get { return isGlobal; }
+            set { SetPropertyValue(nameof(IsGlobal), ref isGlobal, value); }
+        }
+        private Product product;
+        [Association("P-To-C")]
+        public Product Product {
+            get { return product; }
+            set { SetPropertyValue(nameof(Product), ref product, value); }
+        }
+    }
+
+}
--- CS/XPO/FilterLookup/FilterLookup.Module/DatabaseUpdate/Updater.cs
+++ CS/XPO/FilterLookup/FilterLookup.Module/DatabaseUpdate/Updater.cs
@@ -5,6 +5,7 @@ using DevExpress.ExpressApp.Updating;
 using DevExpress.Xpo;
 using DevExpress.ExpressApp.Xpo;
 using DevExpress.Persistent.BaseImpl;
+using FilterLookupListView.Module.BusinessObjects;
 
 namespace FilterLookup.Module.DatabaseUpdate;
 
@@ -15,19 +16,48 @@ public class Updater : ModuleUpdater {
     }
     public override void UpdateDatabaseAfterUpdateSchema() {
         base.UpdateDatabaseAfterUpdateSchema();
-        //string name = "MyName";
-        //DomainObject1 theObject = ObjectSpace.FirstOrDefault<DomainObject1>(u => u.Name == name);
-        //if(theObject == null) {
-        //    theObject = ObjectSpace.CreateObject<DomainObject1>();
-        //    theObject.Name = name;
-        //}
+        Accessory accessory1 = EnsureAccessory("Canon Case Dcc-400", false);
+        Accessory accessory2 = EnsureAccessory("Canon Case Dcc-600", false);
+        Accessory accessory3 = EnsureAccessory("Digitex Secure Digital, 2Gb", true);
+        Accessory accessory4 = EnsureAccessory("A-Data Secure Digital, 2Gb, Super", true);
+        Accessory accessory5 = EnsureAccessory("Unrelated accessory", false);
 
-		//ObjectSpace.CommitChanges(); //Uncomment this line to persist created object(s).
+        Product product1 = EnsureProduct("Canon PowerShot G7", accessory1, accessory2);
+
+        Order order1 = EnsureOrder(10, product1);
+
+        ObjectSpace.CommitChanges();
     }
-    public override void UpdateDatabaseBeforeUpdateSchema() {
-        base.UpdateDatabaseBeforeUpdateSchema();
-        //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
-        //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
-        //}
+    private Accessory EnsureAccessory(string name, bool isGlobal) {
+        Accessory accessory = ObjectSpace.FindObject<Accessory>(CriteriaOperator.Parse("AccessoryName == ?", name));
+        if (accessory == null) {
+            accessory = ObjectSpace.CreateObject<Accessory>();
+            accessory.AccessoryName = name;
+            accessory.IsGlobal = isGlobal;
+            
+        }
+        return accessory;
+    }
+    private Product EnsureProduct(string name, params Accessory[] accessories) {
+        Product product = ObjectSpace.FindObject<Product>(CriteriaOperator.Parse("ProductName == ?", name));
+        if (product == null) {
+            product = ObjectSpace.CreateObject<Product>();
+            product.ProductName = name;
+            foreach (Accessory accessory in accessories) {
+                product.Accessories.Add(accessory);
+            }
+            
+        }
+        return product;
+    }
+    private Order EnsureOrder(int orderId, Product product) {
+        Order order = ObjectSpace.FindObject<Order>(CriteriaOperator.Parse("OrderId == ?", orderId));
+        if (order == null) {
+            order = ObjectSpace.CreateObject<Order>();
+            order.OrderId = orderId;
+            order.Product = product;
+            
+        }
+        return order;
     }
 }
